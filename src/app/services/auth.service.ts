dcbe2251b41a2a75bf87c0ff0a4f678058894456
estuardo-sazo <estuardosazo1206@gmail.com>
2023-08-03 01:32:05 -0600

    Save token in localStorage, validate Route Guard

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
+import { TokenService } from './token.service';
+import { ResponseLogin } from '@models/auth.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   apiUrl = environment.API_URL;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   login(email: string, password: string) {
-    return this.http.post(this.apiUrl + 'auth/login', {
-      email,
-      password,
-    });
+    return this.http
+      .post<ResponseLogin>(this.apiUrl + 'auth/login', {
+        email,
+        password,
+      })
+      .pipe(
+        tap((response) => {
+          this.tokenService.saveToken(response.access_token);
+        })
+      );
   }
 
   register(name: string, email: string, password: string) {
